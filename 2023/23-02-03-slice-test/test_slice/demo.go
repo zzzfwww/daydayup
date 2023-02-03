bb6d717a15da4322cf3a2537de24c719c255e1a1
fabio <zzzfwww@163.com>
2023-02-03 09:24:04 +0800

    Update demo.go

--- 2023/23-02-03-slice-test/test_slice/demo.go
+++ 2023/23-02-03-slice-test/test_slice/demo.go
@@ -5,7 +5,8 @@ import "strings"
 // Split 把字符串s按照给定的分隔符sep进行分割返回字符串切片
 func Split(s, sep string) (result []string) {
 	i := strings.Index(s, sep)
-
+	// 提前分配内存，提示性能
+	result = make([]string, 0, strings.Count(s, sep)+1)
 	for i > -1 {
 		result = append(result, s[:i])
 		// s = s[i+1:]
